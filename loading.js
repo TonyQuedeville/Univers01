db6afc22f2a389df5b15945b82b9172c3d798289
tquedevi <tonyquedeville@gmail.com>
2023-04-06 14:12:06 +0200

    version mobil mode vertical

--- loading.js
+++ loading.js
@@ -6,21 +6,19 @@
 
     var loadimages = 0
 
-    // Sélectionnez l'élément avec le fond d'écran que vous souhaitez attendre
-    var element = document.body;
-
     // Créez une nouvelle instance de l'objet Image
-    var image = new Image();
+    var spaceshipImg = new Image();
 
     // Définissez l'attribut src de l'image sur l'URL de l'image de fond
-    image.src = getComputedStyle(element).backgroundImage.slice(4, -1).replace(/"/g, "");
+    spaceshipImg.src = getComputedStyle(document.body).backgroundImage.slice(4, -1).replace(/"/g, "");
 
     // Attendez que l'image soit chargée avec succès
-    image.addEventListener('load', ()=>{
+    spaceshipImg.addEventListener('load', ()=>{
         loadimages += 1
         if (loadimages === imgnb) OnAllImgLoad()
     });
 
+    // parcours des autres
     for (const img of imgs) {
         if (img.complete && img.naturalHeight !== 0) {loadimages++; continue}
         const l = ()=>{
